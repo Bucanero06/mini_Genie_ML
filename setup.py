9d13d56915b7f288a5942027320273652fb3bbf5
ruben970105 <Bucanero06$1>
2022-09-09 16:58:48 -0400

    _

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='mini_Genie_ML',
+    version='',
+    packages=['mini_Genie', 'mini_Genie.mini_genie_source', 'mini_Genie.mini_genie_source.Utilities',
+              'mini_Genie.mini_genie_source.Indicators', 'mini_Genie.mini_genie_source.Strategies',
+              'mini_Genie.mini_genie_source.Data_Handler', 'mini_Genie.mini_genie_source.Error_Handler',
+              'mini_Genie.mini_genie_source.Optuna_Handler', 'mini_Genie.mini_genie_source.Analysis_Handler',
+              'mini_Genie.mini_genie_source.Run_Time_Handler', 'mini_Genie.mini_genie_source.Equipment_Handler',
+              'mini_Genie.mini_genie_source.mini_Genie_Object', 'mini_Genie.mini_genie_source.Simulation_Handler',
+              'Legendary_Genie', 'mlfinlab_scripts', 'mlfinlab_scripts.src', 'Post_Processing_Genie',
+              'Post_Processing_Genie.post_processing_genie_source',
+              'Post_Processing_Genie.post_processing_genie_source.Utilities',
+              'Post_Processing_Genie.post_processing_genie_source.Filters_Genie',
+              'Post_Processing_Genie.post_processing_genie_source.Analysis_Handler',
+              'Post_Processing_Genie.post_processing_genie_source.Run_Time_Handler',
+              'Post_Processing_Genie.post_processing_genie_source.Equipment_Handler',
+              'Post_Processing_Genie.post_processing_genie_source.Overfitting_Genie'],
+    url='',
+    license='',
+    author='Ruben Fernandez Carbon',
+    author_email='',
+    description=''
+)
